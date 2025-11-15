50905c5dcee7b582b0867b4a76d065dbf29c0737
Caleb <xxy3429924750@126.com>
2025-11-15 21:51:42 +0800

    refactor: improve logging by add file handler

--- core/logging_manager.py
+++ core/logging_manager.py
@@ -1,6 +1,7 @@
 import logging
 import colorlog
 from colorlog.escape_codes import escape_codes
+from logging.handlers import RotatingFileHandler
 
 logging.getLogger("httpx").setLevel(logging.CRITICAL)
 
@@ -9,9 +10,13 @@ escape_codes['orange'] = '\033[38;5;208m'  # 208 是接近橙色的 ANSI 256 色
 
 def get_logger(name: str, color: str):
     logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
     logger.setLevel(logging.DEBUG)
     logger.propagate = False  # 不传播到上级 logger
-    formatter = colorlog.ColoredFormatter(
+    console_formatter = colorlog.ColoredFormatter(
         '%(blue)s%(asctime)s%(reset)s %(log_color)s%(levelname)-8s%(reset)s'
         f'%({color})s[%(name)s]%(reset)s %({color})s%(message)s%(reset)s',
         datefmt='%Y-%m-%d %H:%M:%S',
@@ -26,9 +31,19 @@ def get_logger(name: str, color: str):
         style='%'
     )
 
+    file_formatter = logging.Formatter(
+        '%(asctime)s %(levelname)-8s [%(name)s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    ch.setFormatter(formatter)
+    ch.setFormatter(console_formatter)
+
+    fh = RotatingFileHandler(filename="log.log", maxBytes=10*1024*1024, backupCount=1)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(file_formatter)
 
     logger.addHandler(ch)
+    logger.addHandler(fh)
     return logger
