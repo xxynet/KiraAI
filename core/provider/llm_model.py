746d8389e015a030da9ba251d69d36d5d57d6d01
znq19 <45230005+znq19@users.noreply.github.com>
2026-03-22 13:20:23 +0800

    Update llm_model.py

--- core/provider/llm_model.py
+++ core/provider/llm_model.py
@@ -9,35 +9,17 @@ from core.prompt_manager import Prompt
 
 @dataclass
 class LLMRequest:
-    """LLMRequest object"""
-
-    """message list provided to llm provider"""
     messages: list = field(default_factory=list)
-
-    """Latest user prompt"""
     user_prompt: list[Prompt] = field(default_factory=list)
-
-    """System prompt"""
     system_prompt: list[Prompt] = field(default_factory=list)
-
-    """optional: tool definitions for llm to call"""
     tools: Optional[list[dict]] = None
-
-    """optional: tool functions"""
     tool_funcs: Optional[dict[str, Callable]] = None
-
-    """tool set object"""
     tool_set: Optional[ToolSet] = None
-
-    """controls llm behavior of tool calling"""
     tool_choice: Optional[Literal["auto", "none", "required"]] = None
 
     def __post_init__(self):
         if not self.tool_choice:
-            if self.tools:
-                self.tool_choice = "auto"
-            else:
-                self.tool_choice = "none"
+            self.tool_choice = "auto" if self.tools else "none"
 
     def assemble_prompt(self):
         if self.system_prompt:
@@ -53,33 +35,15 @@ class LLMRequest:
 
 @dataclass
 class LLMResponse:
-    """Content field in chat completion response"""
     text_response: str
-
-    """reasoning content for reasoning models"""
-    reasoning_content: Optional[str] = None
-
-    """agent step index"""
+    reasoning_content: str = ""
     agent_step_index: Optional[int] = None
-
-    """Tool call requests in OpenAI format"""
     tool_calls: list[dict] = field(default_factory=list)
-
-    """Tool results list in OpenAI format, including role assistant & tool"""
     tool_results: list[dict] = field(default_factory=list)
-
     input_tokens: Optional[int] = None
-
     output_tokens: Optional[int] = None
-
-    """Units: seconds"""
     time_consumed: Optional[float] = None
 
-
-@dataclass
-class RerankResult:
-    index: int
-
-    score: float
-
-    text: str
+    def __post_init__(self):
+        if self.reasoning_content is None:
+            self.reasoning_content = ""
