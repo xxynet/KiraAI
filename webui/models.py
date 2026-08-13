ba9e99e1f423e422930669515c66cef22620db16
Caleb <60773145+xxynet@users.noreply.github.com>
2026-08-13 20:15:14 +0800

    feat(webui): enlarge plugin card icons (#241)
    * feat(webui): enlarge plugin card icons
    
    * feat(webui): show plugin store icons

--- webui/models.py
+++ webui/models.py
@@ -270,6 +270,8 @@ class PluginStoreItemResponse(BaseModel):
     category_name: Optional[str] = None
     category_locales: Dict[str, Dict[str, str]] = Field(default_factory=dict)
     repo: Optional[str] = None
+    icon: Optional[str] = None
+    icon_dark: Optional[str] = None
     locales: Dict[str, Dict[str, str]] = Field(default_factory=dict)
     tags: List[str] = Field(default_factory=list)
     stars: int = 0
